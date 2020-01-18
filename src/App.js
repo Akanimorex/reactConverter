b237a0fbd5d712373cb7b68ea65b51d0b4ebcd59
Akanimorex <akanimoime2016@gmail.com>
2020-01-18 15:57:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+
+
+
+
+
+
+class App  extends Component {
+
+constructor(props){
+  super(props);
+  this.state={
+    dollars : '',
+    naira : ''
+  }
+}
+
+convert=()=>{
+   this.setState({naira:this.state.dollars * 362});
+}
+
+handleChange =(event)=>{
+  console.log(event.target.value);
+  (this.setState({dollars: event.target.value}))
+}
+
+
+
+
+
+
+  render (){
+
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='Card'>
+        <h3 style={{color:'white',fontStyle:'italic'}}>CONVERTER</h3>
+        
+        <input type='number' placeholder='Amount in dollars' value={this.state.dollars}  onChange={this.handleChange}  />
+
+        <input type='number' placeholder='Naira Equivalent' value={this.state.naira}  readOnly />
+
+        <input type='submit' value='Convert' onClick={this.convert} />
+
+        
+      </div>
+      
+      
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
